6bd26087f5a5759bf7abf7ce266ef3961471c524
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-03-05 17:19:38 -0500

    Initial Commit

---
+++ InstagramApp/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramApp
+//
+//  Created by Christian Hurtado on 3/5/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+enum AccountState {
+    case existingUser
+    case newUser
+}
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
